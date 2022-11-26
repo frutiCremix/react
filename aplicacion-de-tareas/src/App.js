639f7ad53563cb08acb6783d8fc010e5ab0f9411
Sebastian Oliveto <sebastian.oliveto@hotmail.com>
2022-11-26 00:21:26 -0300

    agregado practica de lista de tareas

---
+++ aplicacion-de-tareas/src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Logo from './logo192.png';
+import ListaDeTareas from './componentes/ListaDeTareas';
+
+function App() {
+  return (
+    <div className="aplicacion-tareas">
+      <div className='logo-contenedor'>
+        <img 
+          src={Logo} 
+          className='logo'
+          alt='logo'
+        />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <ListaDeTareas/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
